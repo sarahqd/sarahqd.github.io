34a96e163d6bf33f06bdedc6b67c4a99d434fe9d
sarahqd <145262472+sarahqd@users.noreply.github.com>
2024-02-18 10:59:01 +0800

    Modify the active behavior of nav-link items

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,16 @@
 // Select all the nav-link elements
 let navLinks = document.querySelectorAll(".nav-link");
 let currentPath = window.location.href;
+let b_active = false;
 // Loop through each nav-link element
 navLinks.forEach(function(navLink) {
     if(navLink.href == currentPath){
         document.querySelector(".nav-link.active").classList.remove("active");
         // Add the active class to the clicked nav-link
         navLink.classList.add("active");
+        b_active = true;
     };
 });
-
+if(!b_active){
+    document.querySelector(".nav-link.active").classList.remove("active");
+}
