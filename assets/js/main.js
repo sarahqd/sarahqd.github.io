e128b4dbbc01c01fa34f11cfd72dac1d1b47c66f
sarahqd <145262472+sarahqd@users.noreply.github.com>
2024-07-29 11:15:25 +0800

    Add search function

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,37 @@ navLinks.forEach(function(navLink) {
 if(!b_active){
     document.querySelector(".nav-link.active").classList.remove("active");
 }
+
+// Function to trigger search on input event
+document.getElementById('inputSearch').addEventListener('input', function(event) {
+    var searchTerm = event.target.value;
+    var simpleJekyllSearchInstance = SimpleJekyllSearch({
+        searchInput: document.getElementById('inputSearch'),
+        resultsContainer: document.getElementById('results-container'),
+        json: '/search.json',
+        searchResultTemplate: '<div><a href="{url}">{title}</a></div>',
+        noResultsText: 'No results found',
+        limit: 10,
+        fuzzy: false
+      });
+    var resultsContainer = document.getElementById('results-container');
+    if(searchTerm){
+        try{
+            simpleJekyllSearchInstance.search(searchTerm);
+            var searchInputRect = event.target.getBoundingClientRect();
+            resultsContainer.style.top = searchInputRect.bottom + window.scrollY + 'px';
+            resultsContainer.style.left = searchInputRect.left + 'px';
+            resultsContainer.style.width = searchInputRect.width + 'px';
+
+            // Check if there's content in the results container
+            // var contentHeight = resultsContainer.scrollHeight;
+            // resultsContainer.style.maxHeight = contentHeight + 'px';
+            resultsContainer.style.display = 'block';
+        }catch(err) {
+            console.error('Error SimpleJekyllSearch:', err);
+        }
+
+    }else{
+        resultsContainer.style.display = 'none';
+    }
+});
